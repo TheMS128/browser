4155e8d174cc48106781ca33273f7cb67e29f9d1
GF <baumann.juergen@posteo.de>
2023-11-24 11:53:57 +0100

    fix: layout improvements

--- app/src/main/java/de/baumann/browser/activity/BrowserActivity.java
+++ app/src/main/java/de/baumann/browser/activity/BrowserActivity.java
@@ -70,7 +70,6 @@ import android.widget.Toast;
 import android.widget.VideoView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.PopupMenu;
