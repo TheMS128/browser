0febe73b811e3081b0191068a0f09b0ceba4c35e
juergen <gf@gf.de>
2025-05-22 20:17:48 +0200

    fix: not switching to default profile, when going back in history

--- app/src/main/java/de/baumann/browser/view/NinjaWebView.java
+++ app/src/main/java/de/baumann/browser/view/NinjaWebView.java
@@ -8,7 +8,6 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
-import android.os.Build;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.util.TypedValue;
